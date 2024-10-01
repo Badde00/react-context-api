669923818b8b45e1691bfce94a2cc0379a4dc756
Iris Flystam <badde00@gmail.com>
2024-10-01 09:44:39 +0200

    part 1 and 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Tweets from './components/Tweets'
-import RightSide from './components/RightSide'
-import defaultTweets from './assets/data/tweets.js'
-import user from './assets/data/user.js'
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Tweets from "./components/Tweets";
+import RightSide from "./components/RightSide";
+import defaultTweets from "./assets/data/tweets.js";
+import user from "./assets/data/user.js";
+import { createContext } from "react";
+
+const TweetContext = createContext();
+const ThemeContext = createContext();
 
 function App() {
-    const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+  const [tweets, setTweets] = useState(defaultTweets);
+  const storedTheme = localStorage.getItem("theme") || "light";
+  const [theme, setTheme] = useState(storedTheme);
 
-    useEffect(() => {
-        theme === 'light'
-          ? document.body.style.backgroundColor = 'white'
-          : document.body.style.backgroundColor = 'black'
-    }, [theme])
+  useEffect(() => {
+    theme === "light"
+      ? (document.body.style.backgroundColor = "white")
+      : (document.body.style.backgroundColor = "black");
 
-    return (
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <TweetContext.Provider value={{ tweets, setTweets, user }}>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
         <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
+          <Header />
+          <Tweets />
+          <RightSide />
         </div>
-    )
+      </ThemeContext.Provider>
+    </TweetContext.Provider>
+  );
 }
 
-export { App };
+export { App, TweetContext, ThemeContext };
